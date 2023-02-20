6b0dc9fe01b2804d6a3fb2e9e51d3570eab43a41
sami2022skipq <abdul.sami.skipq@gmail.com>
2023-02-20 15:42:08 +0500

    new component added

--- new-app/src/App.js
+++ new-app/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
+import Component from './Componet';
 
 function App() {
   return (
     <div className="App">
   
       <h1>hello Again</h1>
-    </div>
+      <Component/>    
+      </div>
   );
 }
 
---
+++ new-app/src/Componet.js
@@ -0,0 +1,11 @@
+// import { Component } from "react"
+
+export default function Component(){
+    return (
+        <div>
+            <h1> hellow from the function</h1>
+            <h1> hellow from the function</h1>
+
+        </div>
+    )
+}
